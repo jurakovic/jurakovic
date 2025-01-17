f206706a557ee3c62c3c417056082323cc4ecc64
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-01-17 22:35:43 +0100

    replace tech placeholder

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-cat << EOF >> "READMEx.md"
+# template
+cat << EOF > READMEx.md
 
 #### Technologies / Tools / Skills
 
+<p align="center">
 __TECH__
+</p>
 
 #### Some Projects
 
@@ -15,3 +18,13 @@ __PROJECTS__
 ![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=jurakovic&layout=compact&hide=java&theme=github_dark)
 
 EOF
+# end of template
+
+# load tech stack
+tech=$(jq -r '.tech[] | "    <img alt=\"tech\" src=\"\(.)\" />"' data.json)
+
+# escale special scaracters (src: https://unix.stackexchange.com/a/519305)
+tech=$(sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<<"$tech")
+
+# replace placeholder in file
+sed -i "s/__TECH__/$tech/" READMEx.md
