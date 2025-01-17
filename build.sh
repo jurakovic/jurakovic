aa9c669d11523feae278d0a50c7322f9aff472cc
jurakovic <17744091+jurakovic@users.noreply.github.com>
2025-01-17 21:40:55 +0100

    initial

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat << EOF >> "READMEx.md"
+
+#### Technologies / Tools / Skills
+
+__TECH__
+
+#### Some Projects
+
+__PROJECTS__
+
+#### Language Stats
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=jurakovic&layout=compact&hide=java&theme=github_dark)
+
+EOF
