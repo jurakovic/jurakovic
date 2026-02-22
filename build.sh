c00ddb4aa972dd4b8c07f0cb1f50c229ad14c55b
jurakovic <17744091+jurakovic@users.noreply.github.com>
2026-02-22 01:20:51 +0100

    Update (#7)

--- build.sh
+++ build.sh
@@ -5,13 +5,16 @@ json="data.json"
 readme="README.md"
 
 currRepo=$(basename -s .git $(git config --get remote.origin.url))
-isReadmeRepo=true
+repoType="readme"
 
-if [ "$currRepo" != "$user" ]; then
-  isReadmeRepo=false
+if [ $currRepo != $user ]; then
+  repoType="githubio"
 fi
 
-if [ "$isReadmeRepo" = false ]; then
+echo "currRepo: $currRepo"
+echo "repoType: $repoType"
+
+if [ "$repoType" = "githubio" ]; then
 
   curl -Sso data.json https://raw.githubusercontent.com/jurakovic/jurakovic/refs/heads/master/data.json
 
@@ -52,46 +55,46 @@ projects=$(jq -r '.projects[]' "$json")
 jq -c '.projects[]' "$json" | while read i; do
   repo=$(echo "$i" | jq -r '.repo')
   icon=$(echo "$i" | jq -r '.icon')
-  pages=$(echo "$i" | jq -r '.pages')
+  readme_show_pages=$(echo "$i" | jq -r '.readme_show_pages')
+  githubio_show_pages=$(echo "$i" | jq -r '.githubio_show_pages')
+  githubio_show_repo=$(echo "$i" | jq -r '.githubio_show_repo')
   mapfile -t descr < <(echo "$i" | jq -r '.description[]')
   mapfile -t readmeDescr < <(echo "$i" | jq -r '.readmeDescr[]?')
-  mapfile -t pagesRepo < <(echo "$i" | jq -r '.pagesRepo[]?')
 
-  if [ "$isReadmeRepo" = true ]; then
+  if [ "$repoType" = "readme" ]; then
 
     echo "- $icon [**$repo**](https://github.com/$user/$repo)" >> $readme
-    
+
     for line in "${descr[@]}"; do
       echo "    - $line" >> $readme
     done
-    
+
     for line in "${readmeDescr[@]}"; do
       echo "    - $line" >> $readme
     done
-    
-    if [ "$pages" == "true" ]
+
+    if [ "$readme_show_pages" == "true" ]
     then
       echo "    - <https://$user.github.io/$repo/>" >> $readme
     fi
 
-  else
+  else # githubio
 
-    if [ "$pages" == "true" ]
+    if [ "$githubio_show_pages" == "true" ]
     then
-      echo "- $icon [$repo](https://$user.github.io/$repo/)" >> $readme
+      echo "- $icon [**$repo**](https://$user.github.io/$repo/)" >> $readme
     else
       echo "- $icon $repo" >> $readme
     fi
-    
+
     for line in "${descr[@]}"; do
       echo "    - $line" >> $readme
     done
-    
-    echo "    - <https://github.com/$user/$repo>" >> $readme
-    
-    for line in "${pagesRepo[@]}"; do
-      echo "    - $line" >> $readme
-    done
+
+    if [ "$githubio_show_repo" == "true" ]
+    then
+      echo "    - <https://github.com/$user/$repo>" >> $readme
+    fi
   fi
 done
 
@@ -108,6 +111,11 @@ cat << EOF >> $readme
   <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/jurakovic/jurakovic/refs/heads/stats/top-langs-light.svg">
   <img src="https://raw.githubusercontent.com/jurakovic/jurakovic/refs/heads/stats/top-langs-dark.svg">
 </picture>
+
+#### Build Status
+
+[![Update stats](https://github.com/jurakovic/jurakovic/actions/workflows/update-stats.yaml/badge.svg)](https://github.com/jurakovic/jurakovic/actions/workflows/update-stats.yaml)
+[![Build Readme](https://github.com/jurakovic/jurakovic.github.io/actions/workflows/build.yaml/badge.svg)](https://github.com/jurakovic/jurakovic.github.io/actions/workflows/build.yaml)
 EOF
 
 if [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
